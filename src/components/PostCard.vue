f09de17b93ee7bbcbd2aa598c0e478d9e7530c0e
liammills1 <liam.mills1511@gmail.com>
2022-04-22 18:16:53 +1000

    bottom navbar done + constants added

---
+++ src/components/PostCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="card">
+    <strong>{{ name }}</strong>
+    <span>Post</span>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'PostCard',
+  props: {
+    name: String,
+    color: String,
+  },
+});
+</script>
+
+<style scoped>
+#card {
+  background: var(--ion-color-off-white);
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.15);
+  border-radius: 18px;
+  padding: 3vw;
+  margin: 1vw 3vw;
+}
+</style>
--- src/views/TabsPage.vue
+++ src/views/TabsPage.vue
@@ -3,29 +3,90 @@
     <ion-tabs>
       <ion-router-outlet></ion-router-outlet>
       <ion-tab-bar slot="bottom">
-        <ion-tab-button tab="home" href="/tabs/home">
-          <ion-icon :icon="homeOutline" />
-          <ion-label>Home</ion-label>
+        <ion-tab-button
+          tab="home"
+          href="/tabs/home"
+          @click="activeTab = 'home'"
+        >
+          <ion-icon
+            v-if="activeTab == 'home'"
+            :icon="homeSelected"
+          />
+          <ion-icon
+            v-else
+            :icon="homeUnselected"
+          />
+          <ion-badge
+            v-if="notifications"
+            color="tertiary"
+          >
+            {{notifications}}
+          </ion-badge>
+          <ion-label>home</ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="leap" href="/tabs/leap">
-          <ion-icon :icon="square" />
-          <ion-label>Leap</ion-label>
+        <ion-tab-button
+          tab="leap"
+          href="/tabs/leap"
+          @click="activeTab = 'leap'"
+        >
+          <ion-icon
+            v-if="activeTab == 'leap'"
+            :icon="leapSelected"
+          />
+          <ion-icon
+            v-else
+            :icon="leapUnselected"
+          />
+          <ion-label>leap</ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="activity" href="/tabs/activity">
-          <ion-icon :icon="ellipse" />
-          <ion-label>Activity</ion-label>
+        <ion-tab-button
+          tab="activity"
+          href="/tabs/activity"
+          @click="activeTab = 'activity'"
+        >
+          <ion-icon
+            v-if="activeTab == 'activity'"
+            :icon="activitySelected"
+          />
+          <ion-icon
+            v-else
+            :icon="activityUnselected"
+          />
+          <ion-label>activity</ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="squad" href="/tabs/squad">
-          <ion-icon :icon="triangle" />
-          <ion-label>Squad</ion-label>
+        <ion-tab-button
+          tab="squad"
+          href="/tabs/squad"
+          @click="activeTab = 'squad'"
+        >
+          <ion-icon
+            v-if="activeTab == 'squad'"
+            :icon="squadSelected"
+          />
+          <ion-icon
+            v-else
+            :icon="squadUnselected"
+          />
+          <ion-label>squad</ion-label>
         </ion-tab-button>
 
-        <ion-tab-button tab="profile" href="/tabs/profile">
-          <ion-icon :icon="personCircleOutline" />
-          <ion-label>Profile</ion-label>
+        <ion-tab-button
+          tab="profile"
+          href="/tabs/profile"
+          @click="activeTab = 'profile'"
+        >
+          <ion-icon
+            v-if="activeTab == 'profile'"
+            :icon="profileSelected"
+          />
+          <ion-icon
+            v-else
+            :icon="profileUnselected"
+          />
+          <ion-label>profile</ion-label>
         </ion-tab-button>
       </ion-tab-bar>
     </ion-tabs>
@@ -35,24 +96,42 @@
 <script>
 import { defineComponent } from 'vue';
 import {
-  IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet,
+  IonTabBar, IonTabButton, IonTabs, IonLabel, IonIcon, IonPage, IonRouterOutlet, IonBadge,
 } from '@ionic/vue';
-import {
-  homeOutline, ellipse, square, triangle, personCircleOutline,
-} from 'ionicons/icons';
+import homeSelected from '../../resources/icons/nav_home_selected.svg';
+import homeUnselected from '../../resources/icons/nav_home_unselected.svg';
+import leapSelected from '../../resources/icons/nav_leap select.svg';
+import leapUnselected from '../../resources/icons/nav_leap unselect.svg';
+import activitySelected from '../../resources/icons/nav_activity select.svg';
+import activityUnselected from '../../resources/icons/nav_activity unselect.svg';
+import squadSelected from '../../resources/icons/nav_squad select.svg';
+import squadUnselected from '../../resources/icons/nav_squad unselect.svg';
+import profileSelected from '../../resources/icons/nav_profile select.svg';
+import profileUnselected from '../../resources/icons/nav_profile unselect.svg';
 
 export default defineComponent({
   name: 'TabsPage',
   components: {
-    IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage, IonRouterOutlet,
+    IonLabel, IonTabs, IonTabBar, IonTabButton, IonIcon, IonPage, IonRouterOutlet, IonBadge,
   },
   setup() {
     return {
-      homeOutline,
-      ellipse,
-      square,
-      triangle,
-      personCircleOutline,
+      homeSelected,
+      homeUnselected,
+      leapSelected,
+      leapUnselected,
+      activitySelected,
+      activityUnselected,
+      squadSelected,
+      squadUnselected,
+      profileSelected,
+      profileUnselected,
+    };
+  },
+  data() {
+    return {
+      notifications: 3,
+      activeTab: 'home',
     };
   },
 });
