2a6a06b2dcab81362c54a7411a155b579a0ee8cf
liammills1 <liam.mills1511@gmail.com>
2022-04-23 11:22:53 +1000

    squad page started

--- src/views/SquadPage.vue
+++ src/views/SquadPage.vue
@@ -1,18 +1,38 @@
 <template>
   <ion-page>
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>Squad Page</ion-title>
-      </ion-toolbar>
-    </ion-header>
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Squad Page</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
-      <ExploreContainer name="Squad page" />
+      <div
+        class="card text-primary bg-secondary text-center"
+        style="margin: 20px 3vw"
+      >
+        <h6>PLACE FOR LOGO</h6>
+        <h2>CISSA</h2>
+      </div>
+      <div class="card text-off-white bg-primary text-center" style="margin-bottom: 15px">
+        <h1 class="text-off-white" style="display: block">54090kg</h1>
+        <h2>CO2 emitted</h2>
+        <h6>38 members</h6>
+      </div>
+      <div class="card text-pink bg-off-white text-center">
+        <h2>leaderboard</h2>
+      </div>
+      <div class="card text-primary bg-off-white">
+        <h5>Squad monthly progress</h5>
+        SPACE FOR CHART
+        <h2>2120kg squad saved</h2>
+        <div class="text-blue spread">
+          <h6>Transport</h6>
+          <h6>900kg</h6>
+        </div>
+        <div class="text-pink spread">
+          <h6>Food</h6>
+          <h6>500kg</h6>
+        </div>
+        <div class="text-tertiary spread">
+          <h6>Energy</h6>
+          <h6>720kg</h6>
+        </div>
+      </div>
     </ion-content>
   </ion-page>
 </template>
@@ -20,14 +40,17 @@
 <script>
 import { defineComponent } from 'vue';
 import {
-  IonPage, IonHeader, IonToolbar, IonTitle, IonContent,
+  IonPage, IonContent,
 } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
 
 export default defineComponent({
   name: 'SquadPage',
   components: {
-    ExploreContainer, IonHeader, IonToolbar, IonTitle, IonContent, IonPage,
+    IonContent, IonPage,
   },
 });
 </script>
+
+<style scoped>
+
+</style>
